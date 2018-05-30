d7d8fcf382c2070c4212da3413b2b42507ad3c5d
gshima <gabrielashima@gmail.com>
2018-05-30 00:59:13 -0300

    Funciona multithread parcialmente

--- src/main.c
+++ src/main.c
@@ -8,43 +8,44 @@
  #include <time.h>
  #include <pthread.h>
 
- #define MAX_WORKERS 4
+ #define MAX_WORKERS 2
  #define ATIVO 1
  #define INATIVO 0
  #define MAX_ELEMENTOS 200000
 
 int vetor[MAX_ELEMENTOS];
 
- // typedef struct {
- //   int N;
- //   int ID;
- // } thread_args;
 
- // pthread_mutex_t trava;
- // int n_workers = 0;
- // pthread_t workers[MAX_WORKERS];
- // int worker_status[MAX_WORKERS];
+ typedef struct {
+   int indice_inicio_esquerda;
+   int indice_inicio_direita;
+   int indice_fim_esquerda;
+   int indice_fim_direita;
+   int ID;
+ } thread_args;
 
- // int fibo(int N) {
- //   if (N<=2) return 1;
- //   else return (fibo(N-1) + fibo(N-2));
- // }
+ pthread_mutex_t trava;
+ int n_workers = 0;
+ pthread_t workers[MAX_WORKERS];
+ int worker_status[MAX_WORKERS];
 
- // void* worker(void *arg) {
- //   thread_args *info = (thread_args *)arg;
- //   int M = fibo(info->N);
- //   printf("Fibo(%d)=%d\n", info->N, M);
- //   pthread_mutex_lock(&trava);
- //   n_workers -= 1;
- //   worker_status[info->ID] = INATIVO;
- //   free(info);
- //   pthread_mutex_unlock(&trava);
- //   return NULL;
- // }
+ void* worker(void *arg) {
+   thread_args *info = (thread_args *)arg;
+   quicksort(info->indice_inicio_esquerda, info->indice_fim_esquerda);
+   quicksort(info->indice_inicio_direita, info->indice_fim_direita);
+   pthread_mutex_lock(&trava);
+   n_workers -= 1;
+   worker_status[info->ID] = INATIVO;
+   free(info);
+   pthread_mutex_unlock(&trava);
+   return NULL;
+ }
 
 
 void quicksort(int primeiro, int ultimo) {
   int pivo, j, temp, i;
+  thread_args *send_args;
+  int k;
 
   if(primeiro < ultimo){
     pivo = primeiro;
@@ -68,10 +69,31 @@ void quicksort(int primeiro, int ultimo) {
     temp = vetor[pivo];
     vetor[pivo] = vetor[j];
     vetor[j] = temp;
-    quicksort(primeiro, j-1);
-    quicksort(j+1, ultimo);
+    if (n_workers >= MAX_WORKERS) {
+      //printf("Muitas tarefas sendo executadas. Ingnorando entrada\n");
+      quicksort(primeiro, j-1);
+      quicksort(j+1, ultimo);
+    } else {
+      pthread_mutex_lock(&trava);
+      //printf("Iniciando nova thread\n");
+      send_args = (thread_args*)malloc(sizeof(thread_args));
+      send_args->indice_inicio_esquerda = primeiro;
+      send_args->indice_fim_esquerda = j-1;
+      send_args->indice_inicio_direita = j+1;
+      send_args->indice_fim_direita = ultimo;
+      /* Procura espaco para thread livre */
+      k = 0;
+      while (worker_status[k] == ATIVO) k++;
+      send_args->ID = k;
+      worker_status[k] = ATIVO;
+      n_workers += 1;
+      //printf("Threads ativas: %d de %d\n", n_workers, MAX_WORKERS);
+      pthread_create(& (workers[j]), NULL, worker, (void*) send_args);
+      pthread_mutex_unlock(&trava);
+    }
   }
 }
+
 
 int main() {
   int qtde_elementos = 0, numero_recebido;
